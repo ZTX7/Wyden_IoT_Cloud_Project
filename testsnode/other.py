aed7bbf25fddcbe647b475858a6d5c6c52b1b0d3
ZTX7 <sineziosrjunior@gmail.com>
2025-05-21 10:17:06 -0300

    fetch: add_files

---
+++ testsnode/other.py
@@ -0,0 +1,39 @@
+from machine import Pin, I2C
+import bme280_float as bme280
+import time, urequests, ujson, network
+
+
+led = Pin(2, Pin.OUT)
+
+def pisca(led):
+    for i in range(3):
+        led.value(0)
+        time.sleep(0.5)
+        led.value(1)
+        time.sleep(0.5)
+
+try:
+
+    pisca(led)
+    i2c = I2C(scl=Pin(5), sda=Pin(4))
+    sensor = bme280.BME280(i2c=i2c)
+
+    while True:
+            
+        temp, pres, umi = sensor.values
+        alt = sensor.dew_point  
+
+        payload = {
+            'node-data': {
+                    'temperatura': temp,
+                    'umidade': umi,
+                    'pressao': pres, 
+                    'altitude': alt,
+                },
+            'LinkPDF': 'https://drive.google.com/file/d/1QZUmZwVIkU8cjRNEMWY8ZiBo4mC6TEYX/view?usp=sharing'
+        }
+        print(payload)
+        time.sleep(1)
+
+except Exception as e:
+    print(f"Error: {e}")
